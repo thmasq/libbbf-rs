685d8d0e20a0276e4c51c9b15ea707828a0c98f1
Thomas Q <thomasqsa@gmail.com>
2026-01-20 21:40:40 -0300

    Wrap functions in the FFI with catch_unwind

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,6 +1,8 @@
 use std::ffi::CStr;
 use std::fs::File;
 use std::os::raw::c_char;
+use std::panic;
+use std::ptr;
 use std::slice;
 
 use crate::builder::BBFBuilder;
@@ -12,21 +14,29 @@ pub struct CBbfReader(BBFReader<File>);
 
 #[unsafe(no_mangle)]
 pub extern "C" fn bbf_builder_new(path: *const c_char) -> *mut CBbfBuilder {
-    let c_str = unsafe { CStr::from_ptr(path) };
-    let str_slice = match c_str.to_str() {
-        Ok(s) => s,
-        Err(_) => return std::ptr::null_mut(),
-    };
+    let result = panic::catch_unwind(|| {
+        if path.is_null() {
+            return ptr::null_mut();
+        }
 
-    let file = match File::create(str_slice) {
-        Ok(f) => f,
-        Err(_) => return std::ptr::null_mut(),
-    };
+        let c_str = unsafe { CStr::from_ptr(path) };
+        let str_slice = match c_str.to_str() {
+            Ok(s) => s,
+            Err(_) => return ptr::null_mut(),
+        };
 
-    match BBFBuilder::new(file) {
-        Ok(builder) => Box::into_raw(Box::new(CBbfBuilder(builder))),
-        Err(_) => std::ptr::null_mut(),
-    }
+        let file = match File::create(str_slice) {
+            Ok(f) => f,
+            Err(_) => return ptr::null_mut(),
+        };
+
+        match BBFBuilder::new(file) {
+            Ok(builder) => Box::into_raw(Box::new(CBbfBuilder(builder))),
+            Err(_) => ptr::null_mut(),
+        }
+    });
+
+    result.unwrap_or(ptr::null_mut())
 }
 
 #[unsafe(no_mangle)]
@@ -36,58 +46,87 @@ pub extern "C" fn bbf_builder_add_page(
     len: usize,
     media_type: u8,
 ) -> u32 {
-    let builder = unsafe { &mut (*builder).0 };
-    let slice = unsafe { slice::from_raw_parts(data, len) };
-    let mtype = BBFMediaType::from(media_type);
+    let result = panic::catch_unwind(|| {
+        if builder.is_null() || data.is_null() {
+            return 0xFFFFFFFF;
+        }
 
-    match builder.add_page(slice, mtype) {
-        Ok(idx) => idx,
-        Err(_) => 0xFFFFFFFF,
-    }
+        let builder_ref = unsafe { &mut (*builder).0 };
+        let slice = unsafe { slice::from_raw_parts(data, len) };
+        let mtype = BBFMediaType::from(media_type);
+
+        match builder_ref.add_page(slice, mtype) {
+            Ok(idx) => idx,
+            Err(_) => 0xFFFFFFFF,
+        }
+    });
+
+    result.unwrap_or(0xFFFFFFFF)
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn bbf_builder_finalize(builder: *mut CBbfBuilder) -> i32 {
-    if builder.is_null() {
-        return -1;
-    }
-    let builder_box = unsafe { Box::from_raw(builder) };
-    match builder_box.0.finalize() {
-        Ok(_) => 0,
-        Err(_) => -1,
-    }
+    let result = panic::catch_unwind(|| {
+        if builder.is_null() {
+            return -1;
+        }
+        let builder_box = unsafe { Box::from_raw(builder) };
+        match builder_box.0.finalize() {
+            Ok(_) => 0,
+            Err(_) => -1,
+        }
+    });
+
+    result.unwrap_or(-1)
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn bbf_reader_open(path: *const c_char) -> *mut CBbfReader {
-    let c_str = unsafe { CStr::from_ptr(path) };
-    let str_slice = match c_str.to_str() {
-        Ok(s) => s,
-        Err(_) => return std::ptr::null_mut(),
-    };
+    let result = panic::catch_unwind(|| {
+        if path.is_null() {
+            return ptr::null_mut();
+        }
 
-    let file = match File::open(str_slice) {
-        Ok(f) => f,
-        Err(_) => return std::ptr::null_mut(),
-    };
+        let c_str = unsafe { CStr::from_ptr(path) };
+        let str_slice = match c_str.to_str() {
+            Ok(s) => s,
+            Err(_) => return ptr::null_mut(),
+        };
 
-    match BBFReader::new(file) {
-        Ok(reader) => Box::into_raw(Box::new(CBbfReader(reader))),
-        Err(_) => std::ptr::null_mut(),
-    }
+        let file = match File::open(str_slice) {
+            Ok(f) => f,
+            Err(_) => return ptr::null_mut(),
+        };
+
+        match BBFReader::new(file) {
+            Ok(reader) => Box::into_raw(Box::new(CBbfReader(reader))),
+            Err(_) => ptr::null_mut(),
+        }
+    });
+
+    result.unwrap_or(ptr::null_mut())
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn bbf_reader_free(reader: *mut CBbfReader) {
-    if !reader.is_null() {
-        unsafe {
-            let _ = Box::from_raw(reader);
+    let _ = panic::catch_unwind(|| {
+        if !reader.is_null() {
+            unsafe {
+                let _ = Box::from_raw(reader);
+            }
         }
-    }
+    });
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn bbf_reader_get_page_count(reader: *mut CBbfReader) -> u32 {
-    let reader = unsafe { &(*reader).0 };
-    reader.footer.page_count.get()
+    let result = panic::catch_unwind(|| {
+        if reader.is_null() {
+            return 0;
+        }
+        let reader_ref = unsafe { &(*reader).0 };
+        reader_ref.footer.page_count.get()
+    });
+
+    result.unwrap_or(0)
 }
